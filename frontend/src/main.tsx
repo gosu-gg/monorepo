e18fa5d3581a344d57ac0e8e8ccd69fafc198dbb
Yohan Tancrez <yohan.tancrez@epita.fr>
2022-03-26 21:56:29 +0100

    added contexts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
+import { Web3Provider } from "./contexts/Web3Context";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Web3Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Web3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
