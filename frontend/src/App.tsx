ffcb6bbad24253002aa4511f677e087f1a257f99
TANCREZ Yohan <Yohan.tancrez@epita.fr>
2022-03-26 02:21:01 +0100

    ✨ Added navigation sidebar (#2)
    * added mui
    
    * added navigation + UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Sidenav from "./components/Sidenav";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>TEST</div>;
+  return (
+    <>
+      <Sidenav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
