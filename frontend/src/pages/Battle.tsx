bbe3973f9b7f56467b5386249e0770012c07587b
Aria Kassaei <arikassaei@gmail.com>
2022-03-27 05:32:59 +0200

    feat: basic profil page

--- frontend/src/pages/Battle.tsx
+++ frontend/src/pages/Battle.tsx
@@ -2,7 +2,7 @@ import Page from "../components/Page";
 import styled from "styled-components";
 
 import CreateGame from "../components/CreateGame";
-import CurrentLobbies from "../components/CurrentLobbies";
+import GameTable from "../components/GameTable";
 
 export default function Battle() {
   return (
@@ -12,7 +12,7 @@ export default function Battle() {
           <CreateGame />
         </SLeftContainer>
         <SRightContainer>
-          <CurrentLobbies />
+          <GameTable tableType="currentLobbies" />
         </SRightContainer>
       </SPageContainer>
     </Page>
